350dc1cac4fc14d3817abc42a6916bc227538f1e
TomJHagey <tomjhagey@gmail.com>
2019-02-12 20:26:03 -0500

    woof

---
+++ WebContent/script.js
@@ -0,0 +1,19 @@
+
+
+var fruit = ["Apple", "Strawberry", "Orange", "kiwi"];
+
+var price = ["1.25", "1.58", "2.34", "2.67"];
+
+var input1 = console.log(fruit[0] + " = $" + price[0]);
+var input2 = console.log(fruit[1] + " = $" + price[1]);
+var input3 = console.log(fruit[2] + " = $" + price[2]);
+var input4 = console.log(fruit[3] + " = $" + price[3]);
+
+for (var i = 0; i < fruit.length; i++) {
+	console.log(fruit[i]);
+}
+
+for (var j = 0; j < price.length; j++) {
+	console.log(price[j]);
+
+}
